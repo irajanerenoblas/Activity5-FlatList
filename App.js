54af27f18ccba143915fb9c4ce112bc4dc40d5e9
Ira Jane Renoblas <irajanerenoblas067@gmail.com>
2023-11-17 22:36:59 +0800

    Initial Change

--- App.js
+++ App.js
@@ -1,20 +1,139 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {
+  FlatList,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  Modal,
+  TouchableOpacity,
+  Alert,
+  View
+} from 'react-native';
+import UserAvatar from 'react-native-user-avatar';
+import Icon from 'react-native-vector-icons/Entypo';
+import USER
+ from './src/Users';
+  export default function App() {
+    const [selectedId, setSelectedId] = useState();
+    const [modalVisible, setModalVisible] = useState(false);
+    const [selectedUser, setSelectedUser] = useState(null);
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const show = (item) =>{
+      setModalVisible(true);
+      setSelectedUser(item);
+    };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const Item = ({item, onPress, backgroundColor, textColor}) => (
+    
+      <TouchableOpacity onPress={onPress} style={[styles.item, {backgroundColor}]}> 
+        <UserAvatar size={50} name={item.name} />
+        <Text style={[styles.title, {color: textColor}]}>{item.name}{'\n'}<Text style={{fontWeight:200}}>{item.place}</Text></Text>
+        <Icon style={{ marginRight: 10, position: 'absolute', marginLeft: 350}} name="dots-three-horizontal" size={30} color="#000" />
+      </TouchableOpacity>
+    
+    );
+
+    const renderItem = ({item}) => {
+      const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#ffff';
+      const color = item.id === selectedId ? 'white' : 'black';
+
+      return (
+        <Item
+          item={item}
+          onPress={() => show(item) }
+          backgroundColor={backgroundColor}
+          textColor={color}
+        />
+      );
+    };
+
+    return (
+      
+      <SafeAreaView style={styles.container}>
+        
+          <Modal
+          animationType="fade"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            setModalVisible(false);
+          }}>
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                  {selectedUser && (
+                    <Text style={{fontSize: 18, fontWeight: '600', fontFamily: 'Tahoma'}}> HELLO, {selectedUser.name}!
+                    </Text>
+                  )}
+                  {selectedUser && (
+                    <Text style={{fontWeight: '100', fontFamily: 'Tahoma', fontSize: 13}}>{selectedUser.place}</Text>
+                  )}
+
+                  <TouchableOpacity onPress={()=> setModalVisible(false)}>
+                    <Text style={{marginTop: 25, fontSize: 15, color: '#695B5D', fontFamily: 'Tahoma'}}>Close</Text>
+                  </TouchableOpacity>
+                    
+                    
+                
+              </View>
+            </View>
+          </Modal>
+
+        <FlatList
+          data={USER}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+          extraData={selectedId}
+        />
+
+      
+      </SafeAreaView>
+    );
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: 20,
+      marginLeft: 0
+      
+    },
+    item: {
+      padding: 10,
+      flexDirection: 'row',
+      alignItems: 'center'
+
+    },
+    centeredView: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: "rgba(255, 255, 255, 0.5)",
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: 'white',
+      borderRadius: 20,
+      padding: 35,
+      alignItems: 'center',
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.50,
+      shadowRadius: 4,
+      elevation: 5,
+    },
+    title: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      fontFamily: 'Tahoma',
+      marginLeft: 20
+    
+    },
+    place: {
+      fontFamily: 'Tahoma',
+      fontSize: 13,
+    }
+  });
---
+++ src/Users.js
@@ -0,0 +1,84 @@
+const USER = [
+    {
+        id: 0,
+        name: "AKITO, YAMADA",
+        place: "Osaka, Japan",
+        
+    },
+    {
+        id: 1,
+        name: "AKIKO, YANO",
+        place: "Hiroshima, Japan",
+
+    },
+    {
+        id: 2,
+        name: "FRANKIE, JONAS",
+        place: "Sapporo, Japan",
+    },
+    {
+        id: 3,
+        name: "GUAN, NING",
+        place: "Fukuoka, Japan",
+    },
+    {
+        id: 4,
+        name: "GRANMAMARE, KAYO",
+        place: "Nara, Japan",
+    },
+    {
+        id: 5,
+        name: "HATTER, SOPHIE",
+        place: "Kyoto, Japan",
+    },
+    {
+        id: 6,
+        name: "KINOSHITA, AKANE",
+        place: "Centro, Japan",
+    },
+    {
+        id: 7,
+        name: "KOIKO, KYO",
+        place: "Suok, Japan",
+    },
+    {
+        id: 8,
+        name: "KUMIKO, SAYSIYO",
+        place: "Bukid, Japan",
+    },
+    {
+        id: 9,
+        name: "YOSHIE, TOKI",
+        place: "Skina, Japan",
+    },
+    {
+        id: 10,
+        name: "YOUNG, MEDAR",
+        place: "Skina, Japan",
+    },
+    {
+        id: 11,
+        name: "WIZARD, HOWL ",
+        place: "Kyoto, Japan",
+    },
+    {
+        id: 12,
+        name: "WEASLEY, RON",
+        place: "Unahan, Japan",
+    },
+    {
+        id: 13,
+        name: "ZAKIRA, YUSAI",
+        place: "Lapas, Japan",
+    },
+    {
+        id: 14,
+        name: "ZACHARY, SHAWN",
+        place: "Skina, Japan",
+    },
+ 
+ 
+ 
+];
+
+export default USER;
